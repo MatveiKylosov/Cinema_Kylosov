a1667b608b3e0ddcd6fae8aafb39eec2df2ff159
MatveiKylosov <matveikyl@mail.ru>
2024-01-07 23:46:16 +0500

    Реализован фильтр.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cinema_Kylosov_Finally.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging; 
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -72,7 +74,88 @@ namespace Cinema_Kylosov_Finally
 
         private void Filter_Click(object sender, RoutedEventArgs e)
         {
+            parent.Children.Clear();
+            Windows.FilterWindow filter = new Windows.FilterWindow(cinema, movies);
+            filter.ShowDialog();
 
+            if (!filter.ActiveFilter) return;
+
+            if (filter.RBCinema.IsChecked == true)
+            {
+                List<Classes.CinemaContext> cinemaSort = new List<Classes.CinemaContext>();
+
+                // Поиск по имени
+                var checkedItems = filter.CinemaList.Items.OfType<CheckBox>().Where(cb => cb.IsChecked == true);
+                var CinemaName = checkedItems.Select(cb => cb.Content.ToString()).ToList();
+
+                if (CinemaName.Any())
+                    cinemaSort.AddRange(cinema.Where(x => CinemaName.Contains(x.CinemaName)));
+
+                // Поиск по количеству мест
+                if (!string.IsNullOrEmpty(filter.NumberOfSeats.Text))
+                {
+                    var seatsSort = cinema.Where(x => x.NumberOfSeats == int.Parse(filter.NumberOfSeats.Text)).ToList();
+                    if (seatsSort.Any())
+                        cinemaSort = cinemaSort.Any() ? cinemaSort.Intersect(seatsSort).ToList() : seatsSort;
+                }
+
+                // Поиск по количеству залов
+                if (!string.IsNullOrEmpty(filter.NumberOfHalls.Text))
+                {
+                    var hallsSort = cinema.Where(x => x.NumberOfHalls == int.Parse(filter.NumberOfHalls.Text)).ToList();
+                    if (hallsSort.Any())
+                        cinemaSort = cinemaSort.Any() ? cinemaSort.Intersect(hallsSort).ToList() : hallsSort;
+                }
+
+                foreach (var x in cinemaSort)
+                {
+                    parent.Children.Add(new Element.Cinema(x));
+                    if (filter.ViewBillboardCinema.IsChecked == true)
+                    {
+                        var matchingBillboards = billboard.Where(i => x.CinemaID == i.CinemaID);
+                        foreach (var billboard in matchingBillboards)
+                            parent.Children.Add(new Element.Billboard(billboard));
+                        
+                    }
+                }
+            }
+
+            else if (filter.RBMovie.IsChecked == true)
+            {
+                List<Classes.MovieContext> MovieSort = new List<Classes.MovieContext>();
+
+                var checkedItems = filter.MovieList.Items.OfType<CheckBox>().Where(cb => cb.IsChecked == true);
+                var MovieName = checkedItems.Select(cb => cb.Content.ToString()).ToList();
+
+                if (MovieName.Any())
+                    MovieSort.AddRange(movies.Where(x => MovieName.Contains(x.MovieName)));
+
+                checkedItems = filter.GenreList.Items.OfType<CheckBox>().Where(cb => cb.IsChecked == true);
+                var Genre = checkedItems.Select(cb => cb.Content.ToString()).ToList();
+
+                var GenreSort = movies.Where(movie => Genre.Any(genre => movie.Genre.Contains(genre))).ToList();
+
+                if(GenreSort.Any())
+                    MovieSort = MovieSort.Any() ? MovieSort.Intersect(GenreSort).ToList() : GenreSort;
+
+                if (!string.IsNullOrEmpty(filter.Duration.Text))
+                {
+                    var durationSort = movies.Where(x => x.Duration == int.Parse(filter.Duration.Text)).ToList();
+                    if(durationSort.Any())
+                        MovieSort = MovieSort.Any() ? MovieSort.Intersect(durationSort).ToList() : durationSort;
+                }
+
+                foreach (var x in MovieSort)
+                {
+                    parent.Children.Add(new Element.Movie(x));
+                    if(filter.ViewBillboardMovie.IsChecked == true)
+                    {
+                        var matchingBillboards = billboard.Where(i => x.MovieID == i.MovieID);
+                        foreach (var billboard in matchingBillboards)
+                            parent.Children.Add(new Element.Billboard(billboard));
+                    }
+                }
+            }
         }
     }
 }
---
+++ Windows/FilterWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cinema_Kylosov_Finally.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для FilterWindow.xaml
+    /// </summary>
+    public partial class FilterWindow : Window
+    {
+        List<Classes.CinemaContext> cinemas;
+        List<Classes.MovieContext> movies;
+
+        public bool ActiveFilter = false;
+
+        public FilterWindow(List<Classes.CinemaContext> cinemas, List<Classes.MovieContext> movies)
+        {
+            InitializeComponent();
+
+            this.cinemas = cinemas;
+            this.movies = movies;
+
+            foreach(var x in cinemas)
+            {
+                CheckBox cb = new CheckBox() { Content = x.CinemaName };
+                cb.Click += CinemaCheckedBox;
+                CinemaList.Items.Add(cb);
+            }
+
+            List <string> genre = new List<string>();
+
+            foreach(var x in movies)
+            {
+                CheckBox cb = new CheckBox() { Content = x.MovieName };
+                cb.Click += MovieCheckedBox;
+                MovieList.Items.Add(cb);
+
+                if (x.Genre.Any(f => f == ','))
+                {
+                    string[] str = x.Genre.Split(',');
+                    foreach (var i in str)
+                        if (!genre.Exists(f => f == i))
+                            genre.Add(i);
+                }
+                else 
+                    GenreList.Items.Add(new CheckBox() { Content = x.Genre });
+            }
+
+            foreach (var x in genre)
+                GenreList.Items.Add(new CheckBox() { Content = x });
+        }
+
+        private void MovieChecked(object sender, RoutedEventArgs e)
+        {
+            FilterCinema.IsEnabled = false;
+            FilterMovie.IsEnabled = true;
+        }
+
+        private void CinemaChecked(object sender, RoutedEventArgs e)
+        {
+            FilterCinema.IsEnabled = true;
+            FilterMovie.IsEnabled = false;
+        }
+
+        private void CinemaCheckedBox(object sender, RoutedEventArgs e)
+        {
+            bool check = CinemaList.Items.OfType<CheckBox>().Any(cb => cb.IsChecked == true);
+
+            ViewBillboardCinema.IsEnabled = check;
+        }
+
+        private void MovieCheckedBox(object sender, RoutedEventArgs e)
+        {
+            bool check = MovieList.Items.OfType<CheckBox>().Any(cb => cb.IsChecked == true);
+
+            ViewBillboardMovie.IsEnabled = check;
+        }
+
+        private void ApplyClick(object sender, RoutedEventArgs e)
+        {
+            ActiveFilter = true;
+            this.Hide();
+        }
+
+        private void ResetClick(object sender, RoutedEventArgs e)
+        {
+            foreach (var x in MovieList.Items)
+                ((CheckBox)x).IsChecked = false;
+
+            foreach (var x in CinemaList.Items)
+                ((CheckBox)x).IsChecked = false;
+
+            foreach (var x in GenreList.Items)
+                ((CheckBox)x).IsChecked = false;
+
+            Duration.Text = NumberOfHalls.Text = NumberOfSeats.Text = "";
+
+            RBMovie.IsChecked = RBCinema.IsChecked = ViewBillboardCinema.IsChecked = ViewBillboardMovie.IsChecked = false;
+        }
+
+        private void NumberOfHalls_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!Char.IsDigit(e.Text, 0))
+                e.Handled = true;
+        }
+
+        private void Duration_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!Char.IsDigit(e.Text, 0))
+                e.Handled = true;
+        }
+
+        private void NumberOfSeats_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!Char.IsDigit(e.Text, 0))
+                e.Handled = true;
+        }
+    }
+}
